bbb1a043886eccca54b038958b0a4890bc6ab0ea
ARTHUR VINICIUS SANTOS SILVA <arthur.vinicius@portoseguro.com.br>
2022-07-19 15:53:42 -0300

    feat: listando repositorios

--- 01-github-explorer/src/components/RepositoryItem.jsx
+++ 01-github-explorer/src/components/RepositoryItem.jsx
@@ -5,7 +5,7 @@ const RepositoryItem = ({ repository }) => {
     <li>
       <strong>{repository.name}</strong>
       <p>{repository.description}</p>
-      <a href={repository.link}>Acessar repositório</a>
+      <a href={repository.html_url}>Acessar repositório</a>
     </li>
   );
 };
--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -2,33 +2,29 @@ import React, { useEffect, useState } from "react";
 import RepositoryItem from "./RepositoryItem";
 import "../styles/repositories.scss";
 
-const repository = {
-  name: "Ignite",
-  description: "Repo RactJS",
-  link: "https://github.com/thuurzz/ignite-reactjs-rocketseat",
-};
-
-//https://api.github.com/users/thuurzz/repos
-
 const RepositoryList = () => {
-  const [repos, setRepos] = useState([]);
+  const [repositories, setRepositories] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     handleRepositories();
   }, []);
 
-  async function handleRepositories(){
+  async function handleRepositories() {
     fetch("https://api.github.com/users/thuurzz/repos")
-      .then(resp => resp.json())
-      .then(data => setRepos(data));
+      .then((resp) => resp.json())
+      .then((data) => setRepositories(data));
   }
 
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
-      <ul>
-        <RepositoryItem repository={repository} />
-      </ul>
+      {repositories.map((repository, index) => {
+        return (
+          <ul key={index}>
+            <RepositoryItem repository={repository} />
+          </ul>
+        );
+      })}
     </section>
   );
 };
